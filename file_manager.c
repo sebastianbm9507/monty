dfb7bcc02a25e79c1a4e1fdf955d5dd66903ef0f
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-14 21:36:08 +0000

    Memory perform

--- file_manager.c
+++ file_manager.c
@@ -1,4 +1,6 @@
 #include "monty.h"
+
+
 /**
  * _open - Function to open the file and check file is accesible
  * @file: file to be open
@@ -6,61 +8,65 @@
  */
 void _open(char *file)
 {
-	FILE *fd;
 
 	if (access(file, R_OK) != 0 || file == NULL)
 	{
 		printf("Error: Can't open file %s\n", file);
+		free_nodes();
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		fd = fopen(file, "r");
-		if (fd == NULL)
+		v->fd = fopen(file, "r");
+		if (v->fd == NULL)
 		{
 			printf("Error: Can't open file %s\n", file);
+			free_nodes();
+			free(v);
 			exit(EXIT_FAILURE);
 		}
-	/** Go to read the file 📄*/
-	_readfile(fd);
-	/* Close the file ❌*/
-	fclose(fd);
+		/** Go to read the file 📄*/
+		_readfile(v->fd);
+		/* Close the file ❌*/
+		fclose(v->fd);
 	}
 }
 /**
  * _readfile - function to read the lines of the opened file
- * @fd: File descriptor of the file
  * Return: Nothing
  */
-void _readfile(FILE *fd)
+void _readfile(void)
 {
-  /** read all the file */
+	/** read all the file */
 	int line;
-	char *linestr = NULL;
 	size_t n;
 	/* Go trough each line ⤵️ */
-	for (line = 1; getline(&linestr, &n, fd) != EOF; line++)
-		_read_lines(linestr, line);
+	for (line = 1; getline(&v->linestr, &n, v->fd) != EOF; line++)
+		_read_lines(line);
 	/** Free the line */
-	free(linestr);
+	free(v->linestr);
 }
 /**
  * _read_lines - Function to separate the line into tokens
- * @linesrt: Line of the file
  * @line: number of the line to read
  * Return: Nothing
  */
-void _read_lines(char *linesrt, int line)
+void _read_lines(int line)
 {
 	char *opcode = NULL;
 	char *value = NULL;
 
-	if (linesrt == NULL)
+
+	if (v->linestr == NULL)
 	{
 		printf("Error: malloc failed\n");
+		free_nodes();
+		fclose(v->fd);
+		free(v->linestr);
+		free(v);
 		exit(EXIT_FAILURE);
 	}
-	opcode = strtok(linesrt, "\n ");
+	opcode = strtok(v->linestr, "\n ");
 	if (opcode == NULL)
 		return;
 	value = strtok(NULL, "\n ");
@@ -76,16 +82,16 @@ void _read_lines(char *linesrt, int line)
  */
 void find_fn(char *opcode, char *val, int line)
 {
-	stack_t *node; /** variable to store the node ⏺ */
-	int i, j, neg = 1; /** counter variables 🔀 */
+	stack_t *node;	   /** variable to store the node ⏺ */
+	int i, j, neg = 1; /** counter v 🔀 */
 	instruction_t list[] = {
-	{"pop", pop_func}, {"push", push_func},	{"pall", pall_func}, {"pint",
-	pint_func}, {"swap", swap_func}, {"add", add_func}, {"nop", nop_func},
-	{"sub", sub_func}, {NULL, NULL}};
+		{"pop", pop_func}, {"push", push_func}, {"pall", pall_func},
+		{"pint", pint_func}, {"swap", swap_func}, {"add", add_func},
+		{"nop", nop_func}, {"sub", sub_func}, {NULL, NULL}};
 	if (opcode[0] == '#')
 		return;
 	for (i = 0; list[i].opcode != NULL; i++)
-	{ /** go through the list to find the function 🔁*/
+	{											 /** go through the list to find the function 🔁*/
 		if (strcmp(opcode, list[i].opcode) == 0) /* Check if the opcode exist ✅ */
 		{
 			if (strcmp(opcode, "push") == 0)
@@ -93,11 +99,11 @@ void find_fn(char *opcode, char *val, int line)
 				if (val != NULL && val[0] == '-')
 					val = val + 1, neg = -1;
 				if (val == NULL)
-				error_handler(5, opcode, line); /* case 5 error */
+					error_handler(5, opcode, line); /* case 5 error */
 				for (j = 0; val[j] != '\0'; j++)
 				{
 					if (isdigit(val[j]) == 0) /* case 5 error */
-					error_handler(5, opcode, line);
+						error_handler(5, opcode, line);
 				}
 				node = create_node(atoi(val) * neg); /* Create node */
 				node = node, list[i].f(&node, line);
@@ -105,11 +111,11 @@ void find_fn(char *opcode, char *val, int line)
 			}
 			else
 			{
-					list[i].f(&head, line);
-					break;
+				list[i].f(&v->head, line);
+				break;
 			}
 		}
 	}
-		if (list[i].opcode == NULL)
+	if (list[i].opcode == NULL)
 		error_handler(3, opcode, line); /* Unknow instruction Error*/
 }
