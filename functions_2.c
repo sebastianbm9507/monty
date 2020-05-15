166a05300ae0e62c393066757fe7a167af7997bd
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-15 00:49:42 +0000

    Add mul function

--- functions_2.c
+++ functions_2.c
@@ -65,3 +65,30 @@ void sub_func(stack_t **stack, unsigned int line)
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
+/**
+ * mul_fun - divs the top two elements of the stack.
+ * @stack: Head of the list
+ * @line: line of file
+ * Return: Nothing
+ */
+void mul_fun(stack_t **stack, unsigned int line)
+{
+	int result;
+	(void)line;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't mul, stack too short\n", line);
+		free_nodes();
+		fclose(v->fd);
+		free(v->linestr);
+		free(v);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack) = (*stack)->next;
+	result = (*stack)->n * (*stack)->prev->n;
+	(*stack)->n = result;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
