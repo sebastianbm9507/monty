2f3de395bbf3f49d2bdb7c6f54f4c73826fc5e31
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-15 02:28:14 +0000

    Adding rotl, rotr

---
+++ functions_4.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * rotl_func - top element of the stack becomes the last one,
+ *  and the second top element of the stack becomes the first one
+ * @stack: Head of the list
+ * @line: line of file
+ * Return: Nothing
+ */
+void rotl_func(stack_t **stack, unsigned int line)
+{
+
+}
+/**
+ * rotr_func - rotates the stack to the bottom.
+ * rotates the stack to the bottom.
+ * @stack: Head of the list
+ * @line: line of file
+ * Return: Nothing
+ */
+void rotr_func(stack_t **stack, unsigned int line)
+{
+
+}
