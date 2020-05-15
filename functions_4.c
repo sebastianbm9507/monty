27f2197d8fa6895835e0dc1d3663e17914bc27c3
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-15 02:47:41 +0000

    Rotr function added

--- functions_4.c
+++ functions_4.c
@@ -24,3 +24,28 @@ void rotl_func(stack_t **stack, unsigned int line)
 	(*stack)->prev->next = NULL;
 	(*stack)->prev = NULL;
 }
+/**
+ * rotr_func - rotates the stack to the bottom.
+ * rotates the stack to the bottom.
+ * @stack: Head of the list
+ * @line: line of file
+ * Return: Nothing
+ */
+void rotr_func(stack_t **stack, unsigned int line)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	tmp = *stack;
+	/* go to last node 🔁 */
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	/* change the last to top ↘️*/
+	tmp->next = *stack;
+	tmp->prev->next = NULL;
+	tmp->prev = NULL;
+	(*stack)->prev = tmp;
+	(*stack) = tmp;
+}
